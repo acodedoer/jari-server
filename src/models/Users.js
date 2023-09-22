838606f6c71c58cf9ec71cf3aecd4ff04c401c2e
acodedoer <misbahu.s.z@gmail.com>
2023-09-22 01:09:53 +0100

    update authentication

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true}, 
-    isAdmin: {type: Boolean, required: true},
-    savedSayings: [{type: mongoose.Schema.Types.ObjectId, ref:"sayings"}]
-});
+    firstname: {type: String, require:true, min:3, max: 50},
+    lastname: {type: String, require:true, min:3, max: 50},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, min:8, required: true},
+    isVerified: {type: Boolean, required:true, default: false},
+    createdSayings: [{type: mongoose.Schema.Types.ObjectId, ref:"sayings"}]},
+    {timestamps: true}    
+);
 
 export const UserModel = mongoose.model("users", UserSchema);
+ 
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,36 +5,48 @@ import bcrypt from 'bcrypt';
 
 const userRouter = express.Router();
 
-userRouter.post("/register", async(req, res)=>{
-    const {username, password} = req.body;
-    const user = await UserModel.findOne({username});
-    
-    if(user){
-        return res.json({message:"User already exists!"});
+userRouter.post("/register", async(req,res) => {
+    try{
+        const {firstname, lastname, email, password} = req.body;
+        const checkEmail = await UserModel.findOne({email});
+        if(checkEmail){
+           return res.status(409).json({"message":"Email in use!"})
+        }
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new UserModel({
+            firstname, lastname, email, password:passwordHash
+        })
+
+        const savedUser = await  newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(err.code).json({message: err.message});
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new UserModel({username, password:hashedPassword, isAdmin:false});
-    await newUser.save();
-
-    res.json({message:"Created new user"})
 })
 
 userRouter.post("/login", async (req, res)=>{
-    const {username, password} = req.body;
-    const user = await UserModel.findOne({username});
+    try{
+        const {email, password} = req.body;
+        const user = await UserModel.findOne({email});
 
-    if(!user){
-        return res.status(401).json({message:"User does not exist!"});
+        if(!user){
+            return res.status(400).json({message:"Username or Password Is Incorrect!"});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(400).json({message:"Username or Password Is Incorrect!"});
+        }
+  
+        const token = jwt.sign({ id:user._id}, process.env.JWT_SIGN_KEY);
+        delete user.password; 
+        res.json({token, userID: user})     
+    } catch( err ){
+        res.status(err.code).json({message: err.message});
     }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if(!isPasswordValid){
-        return res.status(401).json({message:"Username or Password Is Incorrect!"});
-    }
-
-    const token = jwt.sign({ id:user._id}, process.env.JWT_SIGN_KEY);
-    res.json({token, userID: user._id})
 })
  export default userRouter
